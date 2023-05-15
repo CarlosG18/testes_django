ab5a68f6b3bdfad4f7be3c133f28a154bcb4a4a8
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-14 22:16:51 -0300

    commit

--- models_ecommerce/carrinho/urls.py
+++ models_ecommerce/carrinho/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "carrinho"
 urlpatterns = [
     path('create/<int:pk>/', views.create, name="create"),
+    path('add/<int:pk>/', views.add, name="add"),
 ]
--- models_ecommerce/carrinho/views.py
+++ models_ecommerce/carrinho/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from .models import Carrinho, ItemCarrinho
 from cliente.models import Cliente
+from produto.models import Produto
 from django.urls import reverse
 from django.http import HttpResponse,HttpResponseRedirect
 
 def create(request, pk):
+  produtos = Produtos.objects.all()
   cliente = Cliente.objects.filter(pk=pk).get()
   carrinho = Carrinho(cliente=cliente)
   carrinho.save()
-  return HttpResponseRedirect(reverse('cliente:home'))
+  return render(request, "carrinho/index.html", {
+    "Cliente": cliente,
+    "produtos": Produtos,
+  })
+  
+def add(request, pk):
+  return HttpResponse(pk)
